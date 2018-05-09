657a77140fcb099bf8a7dcf888d3d4d8e79e6b34
flatdesign <danil.krosis@gmail.com>
2018-05-09 15:30:14 +0300

    Финальная доработка

--- js/preview.js
+++ js/preview.js
@@ -4,6 +4,25 @@
 	var load = document.querySelector("#upload-file");
 	var uploadOverlay = document.querySelector(".upload-overlay");
 	var uploadCansel = document.querySelector(".upload-form-cancel");
+	var preview = document.querySelector(".effect-image-preview");
+	var FILE_TYPES = ["gif", "jpg", "jpeg", "png"];
+
+	load.addEventListener("change", function() {
+		var file = load.files[0];
+		var fileName = file.name.toLowerCase();
+
+		var matches = FILE_TYPES.some(function(it) {
+			return fileName.endsWith(it);
+		});
+
+		if(matches) {
+				var reader = new FileReader();
+				reader.addEventListener("load", function() {
+					preview.src = reader.result;
+				});
+				reader.readAsDataURL(file);
+		}
+	});
 
 
 	var openUpload = function() {
